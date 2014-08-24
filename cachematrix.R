b3238c4ba35b7362b63f369436ed92ba09e4f1f7
ymedrri <ralfrittner@ralfritersdator.mydomain.example>
2014-08-24 23:08:52 +0200

    Completed assignment
    Function stubs were completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions are provided to create a special matrix object
+## the special object can cache a stored matrix and its invers
+## In case a new matrix is stored the inverse is recalculated
+## when it is requested, otherwise the cached invers is retrived
 
-## Write a short comment describing this function
+## Creates a special matrix object and provides interface for setting
+## getting a matrix and settign and getting it's invers
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invx <- NULL
+  set <- function(y) {
+    x <<- y
+    invx <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(solve) invx <<- solve
+  getinv <- function() invx
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Checks wheter the invers of stored matrix is already calculated and
+## in that case retrieves and returns the cached invers
+## Otherwise inverse is caclulated and then returned. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invx <- x$getinv()
+  if(!is.null(invx)){
+    message("getting cached data")
+    return (invx)
+  }
+  data <- x$get()
+  invx <- solve(data, ...)
+  x$setinv(invx)
+  invx
 }
